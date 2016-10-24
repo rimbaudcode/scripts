6d5a8d89478eafa90dfa57f47b3b097e3392b0d2
art rimbaud <rimbaudcode@gmail.com>
2016-10-24 12:54:35 +0200

    updated

--- install-mint-soft.sh
+++ install-mint-soft.sh
@@ -15,17 +15,26 @@ sudo apt-get upgrade
 sudo apt-get autoremove
 sudo apt-get autoclean
 
-sudo apt-get install 
-sudo apt-get install zsh
+# sudo apt-get install zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sudo apt-get install zsh-syntax-highlighting
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo apt-get install zsh-syntak-highlightning
+# mkdir -p ~/github
+# cd ~/github
+# git clone https://github.com/wofr06/lesspipe.git
+# cd lesspipe
+# ./configure
+# sudo cp lesspipe.sh code2color /usr/local/bin
 
-mkdir -p ~/github
-cd ~/github
-git clone https://github.com/wofr06/lesspipe.git
-cd lesspipe
-./configure
+# cd ~/github
+# git clone https://github.com/rimbaudcode/dotfiles.git
+# git clone https://github.com/rimbaudcode/scripts.git
+# cd ~/github/scripts
+# ./symlinks-mint
+# reboot
+
+# install font-manager
+# install source-code-pro
 
 sudo apt-get install ansiweather
 sudo apt-get install coreutils
@@ -42,11 +51,18 @@ sudo apt-get install ranger
 ranger --copy-config=all
 
 cd ~/github
-git clone https://github.com/rimbaudcode/dotfiles.git
-git clone https://github.com/rimbaudcode/scripts.git
 git clone https://github.com/rimbaudcode/tao.git
 git clone https://github.com/rimbaudcode/tonato.git
 
-sudo apt-get emacs-24-nox
+sudo apt-get install emacs24-nox
+sudo apt-get install libz-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install haskell-stack
+sudo -k
 
-sudo apt-get libz-dev
+cd /tmp
+stack update
+stack new hello simple
+cd hello
+stack setup
+stack build
