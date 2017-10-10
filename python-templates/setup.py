a06b47a00db2acf97a97ea57dd98fcee543f09cf
art rimbaud <rimbaudcode@gmail.com>
2017-10-09 20:53:27 -0500

    added: support for python projects

---
+++ python-templates/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name         = 'package_name',
+      version      = '0.0.1',
+      description  = '',
+      url          = 'http://github.com/rimbaudcode/package_name',
+      author       = 'rimbaudcode',
+      author_email = 'rimbaudcode[aT]gmail(DoT)com',
+      license      = 'BSD3',
+      packages     = ['package_name'],
+      zip_safe     = False)
